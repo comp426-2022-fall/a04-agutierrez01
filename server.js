3588a0dc868ce22c1e71ab3d5900474f276ab5de
Alex <fgutierrez78@yahoo.com>
2022-11-27 15:35:51 -0500

    first attempt.

---
+++ server.js
@@ -0,0 +1,59 @@
+import { roll } from "./lib/roll.js";
+
+import minimist from "minimist"
+import express from "express"
+
+var app = express();
+
+var args = minimist(process.argv.slice(2));
+
+var port = args.port || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+var sides = 6
+var dice = 2
+var rolls = 1
+
+if ("port" in args) {
+	port = args.port
+}
+
+app.get('/app/', function(req, res) {
+	res.status(200).send("200 OK");
+})
+
+app.post('/app/roll/', function(req, res) {
+    const sides = Number(req.body.sides);
+    const dice = Number(req.body.dice);
+    const rolls = Number(req.body.rolls);
+
+    res.send(roll(sides, dice, rolls));
+})
+
+app.get('/app/roll/:sides/', function(req, res) {
+	const sides = Number(req.params.sides);
+	
+	res.status(200).send(roll(sides, 2, 1));
+})
+
+app.get('/app/roll/:sides/:dice/:rolls/', function(req, res) {
+	const sides = Number(req.params.sides);
+        const dice = Number(req.params.dice);
+	const rolls = Number(req.params.rolls);
+
+        res.status(200).send(roll(sides, dice, rolls));
+})
+app.get('/app/roll/:sides/:dice/', function(req, res) {
+        const sides = Number(req.params.sides);
+        const dice = Number(req.params.dice);
+        
+        res.status(200).send(roll(sides, dice, 1));
+})
+app.get('/app/roll/', function(req, res) {
+        res.status(200).send(roll(6, 2, 1));
+})
+app.get('*', function(req, res) {
+	res.status(404).send("404 NOT FOUND");
+})
+
+app.listen(port);
